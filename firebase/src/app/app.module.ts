b811cf8a12912a7bc9ec11b5dee0a7cc2e547a34
Jose Guillermo <jose.guillermo@orbis.com.pe>
2017-03-24 15:09:46 -0500

    first commit

---
+++ firebase/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+
+import {AppComponent} from './app.component';
+import {HeroesComponent} from './components/heroes/heroes.component';
+import {HeroeComponent} from './components/heroe/heroe.component';
+import {APP_ROUTING} from "./app.routes";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroeComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    APP_ROUTING
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
